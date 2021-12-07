d4487e1ab37d546f34f0fe4df3a7c6b3f60d5886
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-08 04:00:08 +0900

    fix 타입 declaration

--- src/components/atoms/Marker/index.tsx
+++ src/components/atoms/Marker/index.tsx
@@ -3,32 +3,32 @@ import { MapMarker } from 'react-kakao-maps-sdk'
 import icon from '../../../assetts/img/airBnb.png'
 
 interface MarkerProps {
-  longitude: number
-  latitude: number
+    longitude: number
+    latitude: number
 }
 
 export const Marker: React.FC<MarkerProps> = props => {
-  return (
-    <MapMarker
-      draggable
-      position={{ lat: props.latitude, lng: props.longitude }}
-      image={{
-        src: icon,
-        size: {
-          width: 35,
-          height: 35,
-        },
-        options: {
-          offset: {
-            x: 10,
-            y: 10,
-          },
-          style: {
-            width: 100,
-            height: 100,
-          },
-        },
-      }}
-    />
-  )
+    return (
+        <MapMarker
+            draggable
+            position={{ lat: props.latitude, lng: props.longitude }}
+            image={{
+                src: icon,
+                size: {
+                    width: 35,
+                    height: 35,
+                },
+                options: {
+                    offset: {
+                        x: 10,
+                        y: 10,
+                    },
+                    style: {
+                        width: 100,
+                        height: 100,
+                    },
+                },
+            }}
+        />
+    )
 }
