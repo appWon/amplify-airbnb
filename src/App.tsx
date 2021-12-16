9a96be8c61f527117f283bac117a347ddd6af1a3
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-16 14:38:59 +0900

    update 리덕스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createGlobalStyle } from 'styled-components'
 import { Amplify } from 'aws-amplify'
+import { Provider } from 'react-redux'
 import reset from 'styled-reset'
 
+import { store } from './store'
 import { Property } from './pages/PropertyList'
+import { PropertyDetail } from './pages/PropertyDetail'
 import awsconfig from './aws-exports'
 
 Amplify.configure(awsconfig)
 
 const App: React.FC = () => {
     return (
-        <BrowserRouter>
-            <GlobalStyle />
-            <Routes>
-                <Route path="/" element={<Property />} />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <GlobalStyle />
+                <Routes>
+                    <Route path="/" element={<Property />} />
+                    <Route path="/property/:id" element={<PropertyDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     )
 }
 
