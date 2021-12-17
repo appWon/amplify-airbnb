0affce1251963ef0a2508c873e6fbbfffacb1b15
jaewon <jangjaewon@jaewonui-MacBookPro.local>
2021-12-18 05:58:26 +0900

    feat 필터항목

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,13 +2,16 @@ import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
 import { SearchState, seachReducer } from './search'
+import { FilterState, filterReducer } from './filter'
 
 export interface reducerType {
     search: SearchState
+    filter: FilterState
 }
 
 const reducer = combineReducers({
     search: seachReducer,
+    filter: filterReducer,
 })
 
 export const store = createStore(reducer, applyMiddleware(thunk))
--- src/store/selector.ts
+++ src/store/selector.ts
@@ -1,4 +1,6 @@
 import { reducerType } from './index'
-import { SearchState } from './search/types'
+import { SearchState } from './search/'
+import { FilterState } from './filter'
 
 export const searchSelector = (state: reducerType): SearchState => state.search
+export const filterSelector = (state: reducerType): FilterState => state.filter
